fbbd1ef9b71b322d6e6d0afea451ca7ef9f43066
Baipyrus <simon.waltemathe@gmail.com>
2024-01-10 13:26:32 +0100

    bugfix: use cmake instead

--- init.lua
+++ init.lua
@@ -193,12 +193,7 @@ require('lazy').setup({
       'nvim-lua/plenary.nvim',
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release; cmake --build build --config Release; cmake --install build --prefix build',
       },
     },
   },
