afb45182b2a6447092fbf4d3c4996e96adc4a3ef
Baipyrus <simon.waltemathe@gmail.com>
2024-03-01 12:43:54 +0100

    use cmake instead of make (windows)

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -22,7 +22,7 @@ return {
         -- `cond` is a condition used to determine whether this plugin should be
         -- installed and loaded.
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable 'cmake' == 1
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
