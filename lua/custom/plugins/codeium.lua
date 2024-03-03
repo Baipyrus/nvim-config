584f9a50b681cee71f6b30fcee8ede3fd8423803
Baipyrus <simon.waltemathe@gmail.com>
2024-03-03 01:47:24 +0100

    install codeium, a free github copilot alternative

---
+++ lua/custom/plugins/codeium.lua
@@ -0,0 +1,10 @@
+-- Set keymap to open chat in browser
+vim.keymap.set('n', '<leader>cc', ':execute codeium#Chat()<Enter>', { desc = '[C]odeium [C]hat' })
+
+return {
+  -- Free Github Copilot alternative
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+  },
+}
