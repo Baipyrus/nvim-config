3b09dcae3ca77c7a3a24f2705d358ac95ddab7a3
Baipyrus <simon.waltemathe@gmail.com>
2024-03-17 14:35:15 +0100

    abort complete using <C-n> for [n]o

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -54,10 +54,11 @@ return {
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
-          -- Accept ([y]es) the completion.
+          -- Accept ([y]es/[n]o) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-n>'] = cmp.mapping.abort(),
 
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
