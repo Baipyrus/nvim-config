6e22ad5fc5faff65a593b269f976e0b31272acaa
Baipyrus <simon.waltemathe@gmail.com>
2024-03-26 08:36:01 +0100

    additional git keymaps

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,5 +1,11 @@
 -- Switch in fugitive.vim status window with the current one
 vim.keymap.set('n', '<leader>gs', '<cmd>Gedit :<cr>', { desc = '[G]it [S]tatus' })
+-- Fetch all changes
+vim.keymap.set('n', '<leader>gF', '<cmd>Git fetch<cr>', { desc = '[G]it [F]etch' })
+-- Push changes
+vim.keymap.set('n', '<leader>gP', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
+-- Pull changes
+vim.keymap.set('n', '<leader>gp', '<cmd>Git pull<cr>', { desc = '[G]it [P]ull' })
 
 return {
   -- Git related plugins
