eea3e0eacc085f8caa7607ef0a150531ed947163
Baipyrus <simon.waltemathe@gmail.com>
2024-03-27 11:24:28 +0100

    moved linter setup to dedicated plugin

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -178,9 +178,6 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
-        'eslint_d',
-        'prettier',
-        'prettierd',
         'node-debug2-adapter',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
