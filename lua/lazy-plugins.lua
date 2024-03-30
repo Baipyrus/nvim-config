2c810036ffe79e4205ea92ea228e52da344a1808
Baipyrus <simon.waltemathe@gmail.com>
2024-03-30 16:23:36 +0100

    isolate markdown plugin to customs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -22,17 +22,6 @@ require('lazy').setup({
   -- Processing-Java
   'sophacles/vim-processing',
 
-  -- Markdown preview plugin
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app; npm install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    ft = { 'markdown' },
-  },
-
   -- Smooth scroll plugin and keymaps
   {
     'karb94/neoscroll.nvim',
