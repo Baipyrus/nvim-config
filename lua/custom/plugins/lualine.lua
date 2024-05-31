20852d016ec33018a518fbdcb31a4b25dfd93fb1
Baipyrus <simon.waltemathe@gmail.com>
2024-05-31 16:53:26 +0200

    revert back to lualine

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = vim.g.have_nerd_font,
+      theme = 'auto',
+      component_separators = '|',
+      section_separators = ' ',
+    },
+  },
+}
