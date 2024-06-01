2067f93aec7736067f10ec40f55e735eb59fc8e2
Baipyrus <simon.waltemathe@gmail.com>
2024-06-01 13:00:49 +0200

    change colorscheme to catppuccin

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     options = {
       icons_enabled = vim.g.have_nerd_font,
-      theme = 'auto',
+      theme = 'catppuccin',
       component_separators = '|',
       section_separators = ' ',
     },
