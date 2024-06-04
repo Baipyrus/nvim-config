29a40276f6c9c46960152754bfc82641a8a757c9
Baipyrus <simon.waltemathe@gmail.com>
2024-06-04 14:23:47 +0200

    configure gopls as golang LSP

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -165,6 +165,7 @@ return {
         tsserver = {},
         pyright = {},
         svelte = {},
+        gopls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -200,6 +201,9 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
         'node-debug2-adapter',
+        'eslint_d',
+        'prettier',
+        'prettierd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
