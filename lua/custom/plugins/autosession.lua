f219313212821357bdce4aa261f2c9a57dbf190d
Baipyrus <simon.waltemathe@gmail.com>
2024-06-19 19:48:41 +0200

    trying auto restoring session plugins

---
+++ lua/custom/plugins/autosession.lua
@@ -0,0 +1,4 @@
+-- Plugin to automatically create and restore simple sessions
+return {
+  'rmagatti/auto-session',
+}
---
+++ lua/custom/plugins/lastplace.lua
@@ -0,0 +1,4 @@
+-- Plugin to intelligently reopen files at last edit position
+return {
+  'farmergreg/vim-lastplace',
+}
