b138a0133657fc4479578f72efd93d601d74a00d
Baipyrus <simon.waltemathe@gmail.com>
2024-07-02 09:59:13 +0200

    remove unwanted autosession plugin

--- lua/custom/plugins/autosession.lua
+++
@@ -1,9 +0,0 @@
--- Plugin to automatically create and restore simple sessions
-return {
-  'rmagatti/auto-session',
-  config = function()
-    require('auto-session').setup {
-      auto_session_enable_last_session = true,
-    }
-  end,
-}
