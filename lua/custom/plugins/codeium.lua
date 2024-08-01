69c8790f6e60f3743f92ebd7801b3852bba1c4f0
Baipyrus <simon.waltemathe@gmail.com>
2024-08-01 11:08:00 +0200

    configure keymaps during plugin config

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,8 +1,9 @@
--- Set keymap to open chat in browser
-vim.keymap.set('n', '<leader>cc', ':execute codeium#Chat()<Enter>', { desc = '[C]odeium [C]hat' })
-
 return {
   -- Free Github Copilot alternative
   'Exafunction/codeium.vim',
   event = 'BufEnter',
+  config = function()
+    -- Set keymap to open chat in browser
+    vim.keymap.set('n', '<leader>cc', ':execute codeium#Chat()<Enter>', { desc = '[C]odeium [C]hat' })
+  end,
 }
--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,14 +1,3 @@
--- Switch in fugitive.vim status window with the current one
-vim.keymap.set('n', '<leader>gs', '<cmd>Gedit :<cr>', { desc = '[G]it [S]tatus' })
--- Fetch all changes
-vim.keymap.set('n', '<leader>gF', '<cmd>Git fetch<cr>', { desc = '[G]it [F]etch' })
--- Push changes
-vim.keymap.set('n', '<leader>gP', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
--- Pull changes
-vim.keymap.set('n', '<leader>gp', '<cmd>Git pull<cr>', { desc = '[G]it [P]ull' })
--- Open history graph
-vim.keymap.set('n', '<leader>gl', '<cmd>Flogsplit<cr><cmd>wincmd k<cr><cmd>q<cr>', { desc = '[G]it [L]og' })
-
 return {
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -20,4 +9,16 @@ return {
     },
     'idanarye/vim-merginal',
   },
+  config = function()
+    -- Switch in fugitive.vim status window with the current one
+    vim.keymap.set('n', '<leader>gs', '<cmd>Gedit :<cr>', { desc = '[G]it [S]tatus' })
+    -- Fetch all changes
+    vim.keymap.set('n', '<leader>gF', '<cmd>Git fetch<cr>', { desc = '[G]it [F]etch' })
+    -- Push changes
+    vim.keymap.set('n', '<leader>gP', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
+    -- Pull changes
+    vim.keymap.set('n', '<leader>gp', '<cmd>Git pull<cr>', { desc = '[G]it [P]ull' })
+    -- Open history graph
+    vim.keymap.set('n', '<leader>gl', '<cmd>Flogsplit<cr><cmd>wincmd k<cr><cmd>q<cr>', { desc = '[G]it [L]og' })
+  end,
 }
