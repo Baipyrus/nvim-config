689f994fbdabd144b019b456a22b2ef70eefe43c
Baipyrus <simon.waltemathe@gmail.com>
2024-08-01 11:29:28 +0200

    unify layz plugin import, allow return multiple plugins per file

--- lua/custom/plugins/lastplace.lua
+++ lua/custom/plugins/lastplace.lua
@@ -1,4 +1,6 @@
 -- Plugin to intelligently reopen files at last edit position
 return {
-  'farmergreg/vim-lastplace',
+  {
+    'farmergreg/vim-lastplace',
+  },
 }
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -9,23 +9,25 @@ vim.api.nvim_create_autocmd('BufLeave', {
 })
 
 return {
-  'stevearc/oil.nvim',
-  -- Optional dependencies
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local oil = require 'oil'
-    oil.setup {
-      view_options = {
-        show_hidden = true,
-      },
-    }
+  {
+    'stevearc/oil.nvim',
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local oil = require 'oil'
+      oil.setup {
+        view_options = {
+          show_hidden = true,
+        },
+      }
 
-    vim.keymap.set('n', '<leader>fe', function()
-      if vim.bo.filetype == 'oil' then
-        oil.close()
-      else
-        oil.open()
-      end
-    end, { desc = '[F]ile [E]xplorer' })
-  end,
+      vim.keymap.set('n', '<leader>fe', function()
+        if vim.bo.filetype == 'oil' then
+          oil.close()
+        else
+          oil.open()
+        end
+      end, { desc = '[F]ile [E]xplorer' })
+    end,
+  },
 }
