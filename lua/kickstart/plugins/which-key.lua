cfaf706a07e7968efafbeec362dd240f1033d976
Baipyrus <simon.waltemathe@gmail.com>
2024-08-07 12:21:08 +0200

    delete unused void register keymaps

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -22,7 +22,7 @@ return {
       -- Document existing key chains
       require('which-key').add {
         { '<leader>c', group = '[C]ode-/[C]hange' },
-        { '<leader>d', group = '[D]ocument/[D]elete', mode = { 'n', 'v' } },
+        { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
@@ -31,12 +31,9 @@ return {
         { '<leader>g', group = '[G]it/[G]lobal', mode = { 'n', 'v' } },
         { '<leader>b', group = '[B]uffer/[B]reakpoint' },
         { '<leader>cd', group = '[D]irectory' },
-        { '<leader>dn', group = '[N]o', mode = { 'n', 'v' } },
         { '<leader>l', group = '[L]ist', mode = { 'n', 'v' } },
         { '<leader>f', group = '[F]ormat/[F]ile', mode = { 'n', 'v' } },
         { '<leader>o', group = '[O]verseer', mode = { 'n', 'v' } },
-        { '<leader>p', group = '[P]aste', mode = { 'v' } },
-        { '<leader>pn', group = '[N]o', mode = { 'v' } },
       }
     end,
   },
