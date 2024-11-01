42d58a856c85d0c1edb756fb7e8b0625406ebc09
Baipyrus <simon.waltemathe@gmail.com>
2024-11-01 23:52:52 +0100

    disable unused builtin plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+}
