cafa3b1b80b29c61e3fc05ed185de646bb1db1a5
Baipyrus <simon.waltemathe@gmail.com>
2024-11-20 10:06:33 +0100

    install html language server for basic functionality

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -185,6 +185,12 @@ return {
         svelte = {},
         ts_ls = {},
         gopls = {},
+        html = {
+          filetypes = {
+            'html',
+            'ejs',
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
