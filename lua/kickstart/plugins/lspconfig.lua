89c445c9011459b24029d7b529bb3628c6b2da40
Baipyrus <simon.waltemathe@gmail.com>
2024-11-20 10:05:05 +0100

    move intelephense into place in "sorted" list

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -178,13 +178,13 @@ return {
       local servers = {
         powershell_es = {},
         rust_analyzer = {},
+        intelephense = {},
         tailwindcss = {},
         omnisharp = {},
         pyright = {},
         svelte = {},
         ts_ls = {},
         gopls = {},
-        intelephense = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
