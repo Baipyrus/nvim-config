37a426e3b3db95aa5197b9aac457c07a75aab00c
Baipyrus <simon.waltemathe@gmail.com>
2025-03-19 10:27:45 +0100

    feat: use --prune flag on git fetch all the time

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -6,7 +6,7 @@ return {
       -- Switch in fugitive.vim status window with the current one
       vim.keymap.set('n', '<leader>gs', '<cmd>Gedit :<cr>', { desc = '[G]it [S]tatus' })
       -- Fetch all changes
-      vim.keymap.set('n', '<leader>gF', '<cmd>Git fetch<cr>', { desc = '[G]it [F]etch' })
+      vim.keymap.set('n', '<leader>gF', '<cmd>Git fetch --prune<cr>', { desc = '[G]it [F]etch' })
       -- Push changes
       vim.keymap.set('n', '<leader>gP', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
       -- Pull changes
