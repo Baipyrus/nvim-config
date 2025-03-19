02a684c65d859070fd8286bf8bf2b3d7ba30ce22
Baipyrus <simon.waltemathe@gmail.com>
2025-03-19 15:03:18 +0100

    chore: update neoscroll for terminal-based scrolling animation

--- lua/custom/plugins/neoscroll.lua
+++ lua/custom/plugins/neoscroll.lua
@@ -4,27 +4,48 @@ return {
     'karb94/neoscroll.nvim',
     enabled = not vim.g.neovide,
     config = function()
-      require('neoscroll').setup {
+      local neoscroll = require 'neoscroll'
+      neoscroll.setup {
         hide_cursor = false,
-        easing_function = nil,
-        respect_scrolloff = true,
       }
 
-      require('neoscroll.config').set_mappings {
-        -- Scroll normally
-        ['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '50' } },
-        ['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '50' } },
-        -- Scroll entire page height
-        ['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '80' } },
-        ['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '80' } },
-        -- Scroll 10% at a time
-        ['<C-y>'] = { 'scroll', { '-0.10', 'false', '25' } },
-        ['<C-e>'] = { 'scroll', { '0.10', 'false', '25' } },
-        -- Jump to top, bottom and center
-        ['zt'] = { 'zt', { '40' } },
-        ['zz'] = { 'zz', { '50' } },
-        ['zb'] = { 'zb', { '40' } },
-      }
+      -- Define modes for all mappings
+      local modes = { 'n', 'v', 'x' }
+
+      -- Scroll normally
+      vim.keymap.set(modes, '<C-u>', function()
+        neoscroll.ctrl_u { duration = 50 }
+      end)
+      vim.keymap.set(modes, '<C-d>', function()
+        neoscroll.ctrl_d { duration = 50 }
+      end)
+
+      -- Scroll entire page height
+      vim.keymap.set(modes, '<C-b>', function()
+        neoscroll.ctrl_b { duration = 80 }
+      end)
+      vim.keymap.set(modes, '<C-f>', function()
+        neoscroll.ctrl_f { duration = 80 }
+      end)
+
+      -- Scroll 10% at a time
+      vim.keymap.set(modes, '<C-y>', function()
+        neoscroll.scroll(-0.10, { move_cursor = false, duration = 25 })
+      end)
+      vim.keymap.set(modes, '<C-e>', function()
+        neoscroll.scroll(0.10, { move_cursor = false, duration = 25 })
+      end)
+
+      -- Jump to top, bottom and center
+      vim.keymap.set(modes, 'zt', function()
+        neoscroll.zt { half_win_duration = 40 }
+      end)
+      vim.keymap.set(modes, 'zz', function()
+        neoscroll.zz { half_win_duration = 50 }
+      end)
+      vim.keymap.set(modes, 'zb', function()
+        neoscroll.zb { half_win_duration = 40 }
+      end)
     end,
   },
 }
