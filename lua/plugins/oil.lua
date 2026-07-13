9001bbe51741de07c71771156561a78a9d7777be
Baipyrus <simon.waltemathe@gmail.com>
2026-07-13 19:27:12 +0200

    plugins: add oil.nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- Fix oil absolute path to relative path conversion
+      vim.api.nvim_create_augroup("OilRelPathFix", {})
+      vim.api.nvim_create_autocmd("BufLeave", {
+        group = "OilRelPathFix",
+        pattern = "oil:///*",
+        callback = function()
+          vim.cmd("cd .")
+        end,
+      })
+
+      local oil = require("oil")
+      oil.setup({
+        view_options = {
+          show_hidden = true,
+        },
+      })
+
+      vim.keymap.set("n", "<leader>fe", function()
+        if vim.bo.filetype == "oil" then
+          oil.close()
+        else
+          oil.open()
+        end
+      end, { desc = "[F]ile [E]xplorer" })
+    end,
+  },
+}
