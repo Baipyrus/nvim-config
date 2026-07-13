39a6d10c92f1a71512dcce10e983a298f17928d9
Baipyrus <simon.waltemathe@gmail.com>
2026-07-13 19:22:08 +0200

    config: remove and add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local del = vim.keymap.del
+
+-- Buffers navigation
+del("n", "<S-h>", { desc = "Prev Buffer" })
+del("n", "<S-l>", { desc = "Next Buffer" })
+
+-- Remove new file keymap
+del("n", "<leader>fn", { desc = "New File" })
+
+-- Remove lazygit keymaps
+if vim.fn.executable("lazygit") == 1 then
+  del("n", "<leader>gg", { desc = "Lazygit (Root Dir)" })
+  del("n", "<leader>gG", { desc = "Lazygit (cwd)" })
+end
+
+-- Remove quit all keymap
+del("n", "<leader>qq", { desc = "Quit All" })
+
+-- Open snacks dashboard keymap
+map("n", "<leader>h", Snacks.dashboard.open, { desc = "Open Dashboard" })
