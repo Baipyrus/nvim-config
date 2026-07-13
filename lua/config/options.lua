b89e5394d8b634e8df35e7d63f9fa33f59e94782
Baipyrus <simon.waltemathe@gmail.com>
2026-07-13 19:22:01 +0200

    config: set editor options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Snacks features
+vim.g.snacks_animate = false
+
+local opt = vim.opt
+
+-- Minimal spacing to keep around the cursor.
+opt.scrolloff = 8
+opt.sidescrolloff = 12
+
+-- Options for `magatti/auto-session`
+opt.sessionoptions =
+  { "blank", "buffers", "curdir", "folds", "help", "tabpages", "winsize", "winpos", "terminal", "localoptions" }
+
+-- Size of an indent
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 2
