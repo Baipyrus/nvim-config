deb7e2d82cf868d25f2bd591d5cd96b6ee84fd8c
Baipyrus <simon.waltemathe@gmail.com>
2026-07-13 19:26:47 +0200

    plugins: configure lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,31 @@
+local icons = LazyVim.config.icons
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", padding = { left = 0, right = 1 } },
+        },
+        lualine_y = {
+          { "progress" },
+        },
+        lualine_z = {
+          { "location" },
+        },
+      },
+    },
+  },
+}
