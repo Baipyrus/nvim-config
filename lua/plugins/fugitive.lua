00882f2194d14314ebd790b7705975b719ed6372
Baipyrus <simon.waltemathe@gmail.com>
2026-08-12 14:16:16 +0200

    chore: add keymap to easily show pretty git log graph

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -11,6 +11,13 @@ return {
       vim.keymap.set("n", "<leader>gP", "<cmd>Git push<cr>", { desc = "[G]it [P]ush" })
       -- Pull changes
       vim.keymap.set("n", "<leader>gp", "<cmd>Git pull<cr>", { desc = "[G]it [P]ull" })
+      -- Show git log graph
+      vim.keymap.set(
+        "n",
+        "<leader>gg",
+        "<cmd>Git ++curwin log --graph --max-count=1000 --pretty='format:%h -%d %s (%cr)' --abbrev-commit --date=relative<cr>",
+        { desc = "[G]it Log [G]raph" }
+      )
     end,
   },
 }
